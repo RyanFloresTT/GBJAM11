b6798a7371f2e996e4471caed3467ca3942e30c0
Robby Klein <hello@robbyk.com>
2023-09-18 22:09:50 -0700

    remove excess

--- Assets/Scripts/Objects/Switch.cs
+++ Assets/Scripts/Objects/Switch.cs
@@ -8,16 +8,6 @@ public class Switch : MonoBehaviour {
     [SerializeField] SpriteRenderer offSprite;
     [SerializeField] LayerMask playerLayer;
 
-    // Start is called before the first frame update
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
-
     private void Toggle() {
         onSprite.enabled = !onSprite.enabled;
     }
