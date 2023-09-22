131c2b50cbf90a3e4b8f7bb479d0b710d0e2fc6e
Robby Klein <hello@robbyk.com>
2023-09-22 12:26:50 -0700

    title screen

---
+++ Assets/Scripts/UI/TitleManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TitleManager : MonoBehaviour {
+    [SerializeField] private SceneLoader sceneLoader;
+    [SerializeField] private UIDocument uiDoc;
+    private VisualElement rootEl;
+    private VisualElement startText;
+    private string activeClass = "active";
+
+    private void OnEnable() {
+        InputHandler.OnStartPressed += HandleStart;
+        InputHandler.OnAPressed += HandleStart;
+        InputHandler.OnBPressed += HandleStart;
+        InputHandler.OnSelectPressed += HandleStart;
+
+        rootEl = uiDoc.rootVisualElement;
+        startText = rootEl.Q("title-start");
+    }
+
+    private void OnDisable() {
+        InputHandler.OnStartPressed -= HandleStart;
+        InputHandler.OnAPressed -= HandleStart;
+        InputHandler.OnBPressed -= HandleStart;
+        InputHandler.OnSelectPressed -= HandleStart;
+    }
+
+    private void HandleStart() {
+        sceneLoader.LoadScene(Scene.Demo);
+        MusicPlayer.Instance.PlaySong(SongName.Dungeon);
+    }
+
+    private void Start() {
+        MusicPlayer.Instance.PlaySong(SongName.Title);
+        StartCoroutine(ToggleActiveClass());
+    }
+
+    private IEnumerator ToggleActiveClass() {
+        bool isActive = true; // Initially set to true
+
+        while (true) {
+            startText.ToggleInClassList(activeClass);
+            isActive = !isActive;
+            yield return new WaitForSeconds(isActive ? 2f : 0.25f);
+        }
+    }
+
+}
